87f2c6fbe72006664bdd49da258ce1b4924c6fc8
BorkStick <4ciiDicBa55@gmail.com>
2025-04-15 00:38:16 -0500

    pushing for live dev

--- vite.config.ts
+++ vite.config.ts
@@ -6,4 +6,8 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  server: {
+    allowedHosts: ['pixelfinity.borked.tools'], // 👈 Add your custom domain here
+    
+  },
 })
